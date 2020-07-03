7766e5823be860f8fda2619adf8adba4625aa478
CC Highman <christopher.highman@microsoft.com>
2020-07-03 04:34:40 -0700

    Clean up scalafmt formatting

--- src/test/scala/com/microsoft/sqlserver/jdbc/spark/bulkwrite/DataSourceTest.scala
+++ src/test/scala/com/microsoft/sqlserver/jdbc/spark/bulkwrite/DataSourceTest.scala
@@ -147,9 +147,112 @@ class DataSourceTest extends SparkFunSuite with Matchers with SharedSQLContext {
           "dataPoolDataSource" -> ""))
     assert(false == DataPoolUtils.isDataPoolScenario(options))
 
-    // DataPool is configured correctly
-    options = new SQLServerBulkJdbcOptions(
-      Map("urL" -> "jdbc:sqlserver://myUrl",
+    options = new SQLServerBulkJdbcOptions(params)
+      assert(options.url                        == params("urL"))
+      assert(options.user                       == params("useR"))
+      assert(options.password                   == params("passworD"))
+      assert(options.dbtable                    == params("dbtablE"))
+      assert(options.databaseName == params("databaseNamE"))
+      assert(options.accessToken == params("accessTokeN"))
+      assert(options.encrypt == params("encrypT"))
+      assert(options.hostNameInCertificate == params("hostNameInCertificatE"))
+      assert(options.lowerBound.get             == params("lowerBounD"))
+      assert(options.upperBound.get             == params("upperBounD"))
+      assert(options.numPartitions.get          == params("numPartitionS").toInt)
+      assert(options.queryTimeout               == params("queryTimeouT").toInt)
+      assert(options.fetchSize                  == params("fetchSizE").toInt)
+      assert(options.isTruncate                 == params("truncatE").toBoolean)
+      assert(options.createTableOptions         == params("createTableOptionS"))
+      assert(options.createTableColumnTypes.get == params("createTableColumnTypeS"))
+      assert(options.customSchema.get           == params("customSchemA"))
+      assert(options.batchSize                  == params("batchsizE").toInt)
+      assert(options.sessionInitStatement.get   == params("sessionInitStatemenT"))
+      assert(options.pushDownPredicate          == params("pushDownPredicatE").toBoolean)
+
+      assert(options.checkConstraints     == params("checkConstraintS").toBoolean)
+      assert(options.fireTriggers         == params("fireTriggerS").toBoolean)
+      assert(options.keepIdentity         == params("keepIdentitY").toBoolean)
+      assert(options.keepNulls            == params("keepNullS").toBoolean)
+      assert(options.tableLock            == params("tableLocK").toBoolean)
+      assert(options.allowEncryptedValueModifications == params("allowEncryptedValueModificationS").toBoolean)
+
+      assert(options.dataPoolDataSource   == params("dataPoolDataSourcE"))
+      assert(options.reliabilityLevel == SQLServerBulkJdbcOptions.NO_DUPLICATES)
+      assert(options.isolationLevel   == Connection.TRANSACTION_SERIALIZABLE)
+      assert(options.driverClass == params("driverClasS"))
+    }
+
+    test("Data pool URL generation") {
+      val urlParams = "database=spark_mssql_db;user=testusera1;password=mypass;encrypt=false;trustServerCertificate=true;"
+      val masterUrl = createURL("master-pv","5678", urlParams)
+      val params = Map(
+        "urL" -> masterUrl,
+        "dbtablE"-> "myTable"
+      )
+      val options = new SQLServerBulkJdbcOptions(params)
+
+      val dphostName =   "dp-0-01"
+      val actualDpURL = createURL(dphostName, "1433", urlParams)
+      val generatedDpUrl = DataPoolUtils.createDataPoolURL(dphostName,options)
+      assert(actualDpURL == generatedDpUrl)
+    }
+
+    test("Multi part tablename test") {
+      val testTableName = "myTable"
+      val testSchemaName = "mySchema"
+      val testDBName = "mydb"
+
+      val urlParams = s"database=$testDBName;user=testusera1;password=mypass;"
+      val masterUrl = createURL("master-pv","5678", urlParams)
+
+      // Test 1 - Check single part table name
+      var params = Map(
+        "urL" -> masterUrl,
+        "dbtable"-> s"$testTableName"
+      )
+      var options = new SQLServerBulkJdbcOptions(params)
+      val (rDb, rSchema, rTable) = BulkCopyUtils.get3PartName(options)
+      assert(rDb == "")
+      assert(rSchema == "dbo")
+      assert(rTable == testTableName)
+
+      // Test 2 - Check 2 part names
+      params = Map(
+        "urL" -> masterUrl,
+        "dbtable"-> s"$testSchemaName.$testTableName"
+      )
+      options = new SQLServerBulkJdbcOptions(params)
+      val (db, schema, table) = BulkCopyUtils.get3PartName(options)
+      assert(db=="")
+      assert(schema==testSchemaName)
+      assert(table==testTableName)
+    }
+
+    def createURL(instanceName:String, port:String, urlParams:String) : String = {
+      s"jdbc:sqlserver://$instanceName:$port;$urlParams"
+    }
+
+    test("Data pool options test") {
+      // DataPool is not configured.
+      var options = new SQLServerBulkJdbcOptions(Map(
+        "urL" -> "jdbc:sqlserver://myUrl", "dbtablE"-> "myTable"))
+      assert(false == DataPoolUtils.isDataPoolScenario(options))
+
+      options = new SQLServerBulkJdbcOptions(Map(
+        "urL" -> "jdbc:sqlserver://myUrl", "dbtablE"-> "myTable",
+        "dataPoolDataSource"->""))
+      assert(false == DataPoolUtils.isDataPoolScenario(options))
+
+      // DataPool is configured correctly
+      options = new SQLServerBulkJdbcOptions(Map(
+        "urL" -> "jdbc:sqlserver://myUrl", "dbtablE"-> "myTable",
+        "dataPoolDataSource"->"myds"))
+      assert(true == DataPoolUtils.isDataPoolScenario(options))
+    }
+
+    test("Correct AAD driver is set when accessToken is specified") {
+      val options = new SQLServerBulkJdbcOptions(
+        Map("urL" -> "jdbc:sqlserver://myUrl",
           "dbtablE" -> "myTable",
           "dataPoolDataSource" -> "myds"))
     assert(true == DataPoolUtils.isDataPoolScenario(options))
