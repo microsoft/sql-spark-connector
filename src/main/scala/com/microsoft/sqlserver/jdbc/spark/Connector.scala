015d3cd9f955ed4322e76312c139560054b44d07
shivsood <shivsood@microsoft.com>
2020-11-12 12:58:22 -0800

    Revert "Add Header and Cleanup (#38)" This reverts commit ed2d2826b82b7dc59b6faf108cac75226d32bdb0.
    Reverting this PR due to test failures from code resturcturing. Lets resubmit this as smaller PRs seperately for Headers,
    Tooling changes and Code Restucturing.

--- src/main/scala/com/microsoft/sqlserver/jdbc/spark/Connector.scala
+++ src/main/scala/com/microsoft/sqlserver/jdbc/spark/Connector.scala
@@ -1,89 +1,90 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.microsoft.sqlserver.jdbc.spark
 
-import java.sql.{Connection, SQLException}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext, SaveMode}
+import java.sql.{Connection, ResultSet, ResultSetMetaData, SQLException}
 
-import com.microsoft.sqlserver.jdbc.spark.connectors.StrategyType
-import com.microsoft.sqlserver.jdbc.spark.utils.BulkCopyUtils.{getColMetaData, mssqlTruncateTable}
-
+import com.microsoft.sqlserver.jdbc.spark.BulkCopyUtils.{getColMetaData, getEmptyResultSet, mssqlTruncateTable}
 import org.apache.spark.internal.Logging
-import org.apache.spark.sql.{DataFrame, SaveMode, SQLContext}
-import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
 import org.apache.spark.sql.execution.datasources.jdbc.JDBCOptions
 import org.apache.spark.sql.execution.datasources.jdbc.JdbcUtils.tableExists
 
 /**
- * Connector class implements the write for supported SaveModes. Each supported
- * SaveMode implements a defined pattern
- * which is supported by writeInParallel, dropTable and createTable interfaces that can
- * be overridden to implement
+ * Connector class implements the write for supported SaveModes. Each supported SaveMode implements a defined pattern
+ * which is supported by writeInParallel, dropTable and createTable interfaces that can be overridden to implement
  * specific write behaviours. All MsSQLSparkConnectors should derive from Connector class.
  *
- * MssSQLSparkConnector supports following connector strategies that use this interface
+ * MssSQLSparkConnector supports following connector strategies that use this inteface
  * 1. DefaultConnector - All executors insert into a user specified table directly.
  * Write to table is not transactional and may results in duplicates in executor restart scenarios.
  *
- * 2. ReliableConnectorStagingTable - Implements 2 Phase commit for a transactional write
- * to user table.
- * In Phase 1 executors do an idempotent insert to Staging tables and Phase
- * 2 the driver combines all
+ * 2. ReliableConnectorStagingTable - Implements 2 Phase commit for a transactional write to user table.
+ * In Phase 1 executors do an idempotent insert to Staging tables and Phase 2 the driver combines all
  * staging tables to transactionally write to user specified table.
  *
- * 3. ReliableConnectorDTC - Will use XA/DTC to implement transactional
- * write with multiple executors.
+ * 3. ReliableConnectorDTC - Will use XA/DTC to implement transactional write with multiple executors.
  * Implementation is TBC as of now.
  *
  */
-abstract class Connector extends Logging with StrategyType {
-  var paramOptions = CaseInsensitiveMap[String](Map.empty)
-
+ abstract class Connector extends Logging {
   /**
-   * write implements a pattern for supported SaveModes.Each supported
-   * SaveMode implements a defined pattern
-   * for consistency e.g. Overwrite mode table is dropped or truncated based
-   * on isTruncateOption.
-   * write is supported by writeInParallel, dropTable and createTable interfaces
-   * that can be overridden
+   * write implements a pattern for supported SaveModes.Each supported SaveMode implements a defined pattern
+   * for consistency e.g. Overwrite mode table is dropped or trucated based on isTruncateOption.
+   * write is supported by writeInParallel, dropTable and createTable interfaces that can be overridden
    * to implement specific write behaviours.
    * @param sqlContext SQLContext passed from spark jdbc datasource framework.
-   * @param mode SaveMode as passed from spark jdbc datasource framework
-   * @param df Raw DataFrame passed from spark data source framework
-   * @param conn Connection as passed down from jdbc framework
-   * @param options SQLServerBulkJdbcOptions passed as a parameter map
+   * @param saveMode as passed from spark jdbc datasource framework
+   * @param rawdf raw dataframe passed from spark data soruce framework
+   * @param parameters User options passed as a parameter map
    */
+
   final def write(
-      sqlContext: SQLContext,
-      mode: SaveMode,
-      df: DataFrame,
-      conn: Connection,
-      options: SQLServerBulkJdbcOptions): Unit = {
+       sqlContext: SQLContext,
+       mode: SaveMode,
+       df: DataFrame,
+       conn: Connection,
+       options: SQLServerBulkJdbcOptions ): Unit = {
     logDebug("write : Entered")
     try {
-      if (tableExists(conn, options)) processExistingTable(sqlContext, conn, mode, df, options)
-      else processNewTable(sqlContext, conn, df, options)
-    } finally logDebug("write : Exiting")
+      if (tableExists(conn, options)) {
+        mode match {
+          case SaveMode.Overwrite =>
+            if(options.isTruncate) {
+              logInfo(s"Overwriting with truncate for table '${options.dbtable}'")
+              val colMetaData = getColMetaData(df, conn, sqlContext, options, true)
+              mssqlTruncateTable(conn, options.dbtable)
+              writeInParallel(df, colMetaData, options, sqlContext.sparkContext.applicationId)
+            }
+            else {
+              logInfo(s"Overwriting without truncate for table '${options.dbtable}'")
+              dropTable(conn, options.dbtable, options)
+              createTable(conn, df, options)
+              val colMetaData = getColMetaData(df, conn, sqlContext, options, false)
+              writeInParallel(df, colMetaData, options, sqlContext.sparkContext.applicationId)
+            }
+          case SaveMode.Append =>
+            logInfo(s"Appending to table '${options.dbtable}'")
+            val colMetaData = getColMetaData(df, conn, sqlContext, options,true)
+            writeInParallel(df, colMetaData, options, sqlContext.sparkContext.applicationId)
+          case SaveMode.ErrorIfExists =>
+            throw new SQLException(
+              s"""Error with SaveMode 'ErrorIfExists':
+                                Table '${options.dbtable}' already exists""")
+          case SaveMode.Ignore =>
+            logInfo(s"Table '${options.dbtable}' already exists and SaveMode is Ignore")
+        }
+      } else {
+        logDebug(s"Table '${options.dbtable} does not exist'")
+        createTable(conn, df, options)
+        val colMetaData = getColMetaData(df, conn, sqlContext, options,false)
+        writeInParallel(df, colMetaData, options, sqlContext.sparkContext.applicationId)
+      }
+    } finally {
+      logDebug("write : Exiting")
+    }
   }
 
   /**
-   * writeInParallel distributes the given dataframe to executors to write.
-   * Respective connector implementations
+   * writeInParallel distributes the given dataframe to executors to write. Respective connector implementations
    * can override this method to implement specific parallelization.
    * @param df dataframe to write
    * @param colMetaData column meta data of the table.
@@ -91,77 +92,32 @@ abstract class Connector extends Logging with StrategyType {
    * @param appId of the spark application.
    */
   def writeInParallel(
-      df: DataFrame,
-      colMetaData: Array[ColumnMetadata],
-      options: SQLServerBulkJdbcOptions,
-      appId: String)
+        df: DataFrame,
+        colMetaData: Array[ColumnMetadata],
+        options: SQLServerBulkJdbcOptions,
+        appId: String): Unit
 
   /**
-   * createTable interface. Respective connector implementations can override
-   * this to implement specific functionality
+   * createTable interface. Respective connector implementations can override this to implement specific functionality
    * e.g. data pool connectors  create external table as opposed to data base table.
    * @param conn JDBCConnection to use   *
    * @param df dataframe to write
    * @param options user provided options.
    */
-  def createTable(conn: Connection, df: DataFrame, options: SQLServerBulkJdbcOptions)
+  def createTable(
+        conn: Connection,
+        df: DataFrame,
+        options: SQLServerBulkJdbcOptions) : Unit
 
   /**
-   * dropTable interface. Respective connector implementations can override this to
-   * implement specific functionality
+   * dropTable interface. Respective connector implementations can override this to implement specific functionality
    * e.g. data pool connectors works with external tables that need to have specific drop syntax.
    * @param conn JDBCConnection to use   *
    * @param dbtable dbtable
    * @param options user provided options.
    */
-  def dropTable(conn: Connection, dbtable: String, options: JDBCOptions): Unit
-
-  def processNewTable(
-      sqlContext: SQLContext,
-      conn: Connection,
-      df: DataFrame,
-      options: SQLServerBulkJdbcOptions): Unit = {
-    logDebug(s"Table '${options.dbtable} does not exist'")
-    createTable(conn, df, options)
-    val colMetaData = getColMetaData(df, conn, sqlContext, options, true)
-    writeInParallel(df, colMetaData, options, sqlContext.sparkContext.applicationId)
-  }
-
-  def processExistingTable(
-      sqlContext: SQLContext,
-      conn: Connection,
-      mode: SaveMode,
-      df: DataFrame,
-      options: SQLServerBulkJdbcOptions): Unit = {
-    mode match {
-      case SaveMode.Overwrite =>
-        if (options.isTruncate) {
-          logInfo(s"Overwriting with truncate for table '${options.dbtable}'")
-          mssqlTruncateTable(conn, options.dbtable)
-        } else {
-          logInfo(s"Overwriting without truncate for table '${options.dbtable}'")
-          dropTable(conn, options.dbtable, options)
-          createTable(conn, df, options)
-        }
-
-      case SaveMode.Append =>
-        logInfo(s"Appending to table '${options.dbtable}'")
-
-      case SaveMode.ErrorIfExists =>
-        throw new SQLException(s"""Error with SaveMode 'ErrorIfExists':
-                                Table '${options.dbtable}' already exists""")
-
-      case SaveMode.Ignore =>
-        logInfo(s"Table '${options.dbtable}' already exists and SaveMode is Ignore")
-        return
-    }
-    val colMetaData = getColMetaData(df, conn, sqlContext, options, true)
-    writeInParallel(df, colMetaData, options, sqlContext.sparkContext.applicationId)
-  }
-  def params(): CaseInsensitiveMap[String] = paramOptions
-
-  def params(params: Map[String, String]): this.type = {
-    paramOptions = CaseInsensitiveMap[String](params)
-    this
-  }
+  def dropTable(
+        conn: Connection,
+        dbtable: String,
+        options: JDBCOptions): Unit
 }
--- src/main/scala/com/microsoft/sqlserver/jdbc/spark/SQLServerBulkJdbcOptions.scala
+++ src/main/scala/com/microsoft/sqlserver/jdbc/spark/SQLServerBulkJdbcOptions.scala
@@ -1,29 +1,14 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.microsoft.sqlserver.jdbc.spark
 
 import java.sql.Connection
 
 import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
-import org.apache.spark.sql.execution.datasources.jdbc.JdbcOptionsInWrite
+import org.apache.spark.sql.execution.datasources.jdbc.{
+  JDBCOptions,
+  JdbcOptionsInWrite
+}
 
-case class SQLServerBulkJdbcOptions(params: CaseInsensitiveMap[String])
+class SQLServerBulkJdbcOptions(val params: CaseInsensitiveMap[String])
     extends JdbcOptionsInWrite(params) {
 
   def this(params: Map[String, String]) = this(CaseInsensitiveMap(params))
@@ -42,46 +27,47 @@ case class SQLServerBulkJdbcOptions(params: CaseInsensitiveMap[String])
   // A non-empty value indicates the name of a data source whose location is
   // the data pool that the user wants to write to. This data source will
   // contain the user's external table.
-  val dataPoolDataSource: String = params.getOrElse("dataPoolDataSource", null)
+  val dataPoolDataSource = params.getOrElse("dataPoolDataSource", null)
 
   // In the standard Spark JDBC implementation, the default isolation level is
   // "READ_UNCOMMITTED," but for SQL Server, the default is "READ_COMMITTED"
-  override val isolationLevel: Int =
+  override val isolationLevel =
     params.getOrElse("mssqlIsolationLevel", "READ_COMMITTED") match {
       case "READ_UNCOMMITTED" => Connection.TRANSACTION_READ_UNCOMMITTED
-      case "READ_COMMITTED" => Connection.TRANSACTION_READ_COMMITTED
-      case "REPEATABLE_READ" => Connection.TRANSACTION_REPEATABLE_READ
-      case "SERIALIZABLE" => Connection.TRANSACTION_SERIALIZABLE
-      case "SNAPSHOT" => Connection.TRANSACTION_READ_COMMITTED + 4094
+      case "READ_COMMITTED"   => Connection.TRANSACTION_READ_COMMITTED
+      case "REPEATABLE_READ"  => Connection.TRANSACTION_REPEATABLE_READ
+      case "SERIALIZABLE"     => Connection.TRANSACTION_SERIALIZABLE
+      case "SNAPSHOT"         => Connection.TRANSACTION_READ_COMMITTED + 4094
     }
 
-  val reliabilityLevel: Int = params.getOrElse("reliabilityLevel", "BEST_EFFORT") match {
-    case "BEST_EFFORT" => SQLServerBulkJdbcOptions.BEST_EFFORT
-    case "NO_DUPLICATES" => SQLServerBulkJdbcOptions.NO_DUPLICATES
-  }
+  val reliabilityLevel =
+    params.getOrElse("reliabilityLevel", "BEST_EFFORT") match {
+      case "BEST_EFFORT"   => SQLServerBulkJdbcOptions.BEST_EFFORT
+      case "NO_DUPLICATES" => SQLServerBulkJdbcOptions.NO_DUPLICATES
+    }
 
   // batchSize is already defined in JDBCOptions superclass
-  val checkConstraints: Boolean = params.getOrElse("checkConstraints", "false").toBoolean
-  val fireTriggers: Boolean = params.getOrElse("fireTriggers", "false").toBoolean
-  val keepIdentity: Boolean = params.getOrElse("keepIdentity", "false").toBoolean
-  val keepNulls: Boolean = params.getOrElse("keepNulls", "false").toBoolean
-  val tableLock: Boolean = params.getOrElse("tableLock", "false").toBoolean
-  val allowEncryptedValueModifications: Boolean =
+  val checkConstraints = params.getOrElse("checkConstraints", "false").toBoolean
+  val fireTriggers = params.getOrElse("fireTriggers", "false").toBoolean
+  val keepIdentity = params.getOrElse("keepIdentity", "false").toBoolean
+  val keepNulls = params.getOrElse("keepNulls", "false").toBoolean
+  val tableLock = params.getOrElse("tableLock", "false").toBoolean
+  val allowEncryptedValueModifications =
     params.getOrElse("allowEncryptedValueModifications", "false").toBoolean
+
 
   val schemaCheckEnabled =
     params.getOrElse("schemaCheckEnabled", "true").toBoolean
 
   // Not a feature
   // Only used for internally testing data idempotency
-  val testDataIdempotency: Boolean = params.getOrElse("testDataIdempotency", "false").toBoolean
+  val testDataIdempotency =
+    params.getOrElse("testDataIdempotency", "false").toBoolean
 
-  val dataPoolDistPolicy: String = params.getOrElse("dataPoolDistPolicy", "ROUND_ROBIN")
+  val dataPoolDistPolicy = params.getOrElse("dataPoolDistPolicy", "ROUND_ROBIN")
 }
 
 object SQLServerBulkJdbcOptions {
   val BEST_EFFORT = 0
   val NO_DUPLICATES = 1
-  val InstanceStrategy: String = "InstanceStrategy"
-  val DataPoolStrategy: String = "DataPoolStrategy"
 }
